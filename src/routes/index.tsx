ed0b71163da40e9b52e86fc42cf9705841a016cf
Maddock <sowtag@gmail.com>
2026-02-25 15:46:44 -0300

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,9 +17,8 @@ function Home() {
 
   return (
     <div className="p-2">
-      <h3>Welcome Home!!!</h3>
+      <h3>Welcome Home!</h3>
       <p>{data.message}</p>
-      <p>{data.myVar}</p>
     </div>
   )
 }
